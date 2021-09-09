833bf96794f335aa30d8049836b5bc3fcf87167e
unknown <dtulpa16@yahoo.com>
2021-09-09 15:05:45 -0500

    Initial commit. Created react app

---
+++ src/Components/App.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './App.css'
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <React.Fragment>
+
+                
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default App;
